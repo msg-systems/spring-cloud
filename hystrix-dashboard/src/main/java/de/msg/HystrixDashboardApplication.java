a1cdf3b541dbb845c3fc63a95f37847c06433ecb
Rafael Kansy <rafael.kansy@msg-systems.com>
2015-12-10 11:06:12 +0100

    Created hystrix-dashboard Maven module.
    Change-Id: Idefb7e2a24f2a07a21791a06082d7482c886a19c

---
+++ hystrix-dashboard/src/main/java/de/msg/HystrixDashboardApplication.java
@@ -0,0 +1,24 @@
+package de.msg;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@EnableHystrixDashboard
+@Controller
+public class HystrixDashboardApplication {
+	
+	@RequestMapping("/")
+	public String home() {
+		return "forward:/hystrix";
+	}
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(HystrixDashboardApplication.class).web(true).run(args);
+    }
+}
