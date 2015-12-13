fdec1987398cfef3073395a14a8bc6d90867ac1e
Rafael Kansy <rafael.kansy@msg-systems.com>
2015-12-13 21:35:34 +0100

    Added JavaDoc to EurekaServerApplication.
    Change-Id: Ia9c45d8254f44c3036052d936e8b481661525e9a

--- eureka-server/src/main/java/de/msg/EurekaServerApplication.java
+++ eureka-server/src/main/java/de/msg/EurekaServerApplication.java
@@ -7,7 +7,11 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
-
+    /**
+     * Main method as entry point of {@link EurekaServerApplication}.
+     *
+     * @param args The command line arguments.
+     */
     public static void main(String[] args) {
         new SpringApplicationBuilder(EurekaServerApplication.class).web(true).run(args);
     }
