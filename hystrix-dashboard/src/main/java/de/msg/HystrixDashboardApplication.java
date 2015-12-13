a4c089699c51450e8d7044d436bc704f5df0c60e
Rafael Kansy <rafael.kansy@msg-systems.com>
2015-12-13 21:36:01 +0100

    Added JavaDoc to HystrixDashboardApplication.
    Change-Id: I8db51d6cfff7b133e4bfb23d4ca2098d83247f1f

--- hystrix-dashboard/src/main/java/de/msg/HystrixDashboardApplication.java
+++ hystrix-dashboard/src/main/java/de/msg/HystrixDashboardApplication.java
@@ -22,6 +22,11 @@ public class HystrixDashboardApplication {
         return "forward:/hystrix";
     }
 
+    /**
+     * Main method as entry point of {@link HystrixDashboardApplication}.
+     *
+     * @param args The command line arguments.
+     */
     public static void main(String[] args) {
         new SpringApplicationBuilder(HystrixDashboardApplication.class).web(true).run(args);
     }
