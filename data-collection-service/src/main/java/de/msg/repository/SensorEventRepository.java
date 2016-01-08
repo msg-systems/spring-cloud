46dc7bafb0a89f8967ff271c2dce6dee4712ddf9
Rafael Kansy <rafael.kansy@msg-systems.com>
2016-01-09 00:08:43 +0100

    Added spring-data @Param annotations to SensorEventRepository.
    Change-Id: Ia7f7a657eab36ff8229b4b42d202afe198ad77b2

--- data-collection-service/src/main/java/de/msg/repository/SensorEventRepository.java
+++ data-collection-service/src/main/java/de/msg/repository/SensorEventRepository.java
@@ -3,7 +3,10 @@ package de.msg.repository;
 import de.msg.model.SensorEvent;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.Set;
 
 /**
  * An simple {@link MongoRepository} to persist {@link SensorEvent} data. In more sophisticated scenarios you would use a NoSQL database like Apache Cassandra.
@@ -20,5 +23,13 @@ public interface SensorEventRepository extends MongoRepository<SensorEvent, Stri
      * @param sensorValue The value of the sensor
      * @return The {@link SensorEvent}
      */
-    SensorEvent findByCarAndSensorNameAndSensorValue(long car, String sensorName, String sensorValue);
+    SensorEvent findByCarAndSensorNameAndSensorValue(@Param("car") long car, @Param("sensorName") String sensorName, @Param("sensorValue") String sensorValue);
+
+    /**
+     * Finds {@link SensorEvent} entities by <tt>car</tt>.
+     *
+     * @param car The id of the car
+     * @return A {@link Set} of {@link SensorEvent} instances.
+     */
+    Set<SensorEvent> findByCar(@Param("car") long car);
 }
